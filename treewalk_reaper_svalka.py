f4e2047971494bd2fa1c2d71f210af8d0e1b430e
OmOmOmg <132565415+OmOmOmg@users.noreply.github.com>
2023-07-16 11:48:54 +0200

    Update treewalk_reaper_svalka.py

--- treewalk_reaper_svalka.py
+++ treewalk_reaper_svalka.py
@@ -1,19 +1,19 @@
 import os
 import shutil
 
-for root, dirs, files in os.walk('.', topdown=False):    
+for root, dirs, files in os.walk('.', topdown=False): 
 
-    for j in files:
+    x = root
+
+    if root == '.':             
+        x = "minimal_hardcore" 
         
-        x = root
+    os.makedirs(f"/home/nick/tech/python/sort_reaper/output_project_dirs/{os.path.basename(x)}", exist_ok=True)
 
-        if root == '.':             
-            x = "minimal_hardcore"          
-                            
+    for j in files:               
+                           
         source = f"{root}/{j}"
-
-        os.makedirs(f"/home/nick/tech/python/sort_reaper/output_project_dirs/{os.path.basename(x)}", exist_ok=True)
-
+        
         project_folder_dest = f"/home/nick/tech/python/sort_reaper/output_project_dirs/{os.path.basename(x)}/{j}"                    
                        
         shutil.copyfile(source, project_folder_dest)
